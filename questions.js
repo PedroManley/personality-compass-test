7133e51e843a1810400dfae4cbd64848b7f3614a
PedroManley <123265557+PedroManley@users.noreply.github.com>
2025-11-22 20:42:46 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,13 +1,22 @@
-questions =  [
-    {"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"peter": 1}},
-    {"id": 1, "question": "Charity is a better way of helping those in need than social welfare.", "effects": {"peter": 1}},
-    {"id": 2, "question": "Wages are always fair, as employers know best what a worker's labour is worth.", "effects": {"peter": 1}},
-    {"id": 3, "question": "It is \"human nature\" to be greedy.", "effects": {"peter": 1}},
-    {"id": 4, "question": "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism don't exist anymore.", "effects": {"peter": 1}},
-    {"id": 5, "question": "Communism is an ideal that can never work in practice.", "effects": {"max": 1}},
-    {"id": 6, "question": "Taxation of the wealthy is a bad idea, society would be better off without it.", "effects": {"max": 1}},
-    {"id": 7, "question": "The harder you work, the more you progress up the social ladder.", "effects": {"max": 1}},
-    {"id": 8, "question": "Organisations and corporations cannot be trusted and need to be regulated by the government.", "effects": {"max": -1}},
-    {"id": 9, "question": "A government that provides for everyone is an inherently good idea.", "effects": {"max": -1}},
-    {"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"max": -1}},
-];
+// questions =  [
+//     {"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"peter": 1}},
+//     {"id": 1, "question": "Charity is a better way of helping those in need than social welfare.", "effects": {"peter": 1}},
+//     {"id": 2, "question": "Wages are always fair, as employers know best what a worker's labour is worth.", "effects": {"peter": 1}},
+//     {"id": 3, "question": "It is \"human nature\" to be greedy.", "effects": {"peter": 1}},
+//     {"id": 4, "question": "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism don't exist anymore.", "effects": {"peter": 1}},
+//     {"id": 5, "question": "Communism is an ideal that can never work in practice.", "effects": {"max": 1}},
+//     {"id": 6, "question": "Taxation of the wealthy is a bad idea, society would be better off without it.", "effects": {"max": 1}},
+//     {"id": 7, "question": "The harder you work, the more you progress up the social ladder.", "effects": {"max": 1}},
+//     {"id": 8, "question": "Organisations and corporations cannot be trusted and need to be regulated by the government.", "effects": {"max": -1}},
+//     {"id": 9, "question": "A government that provides for everyone is an inherently good idea.", "effects": {"max": -1}},
+//     {"id": 10, "question": "The current welfare system should be expanded to further combat inequality.", "effects": {"max": -1}},
+// ];
+
+questions = [
+    {id: 0, question: "Someone took your phone at lunch. What do you do?", choices: [
+        {
+            answer: "Immediately start screaming for an authority.",
+            effects: {"max": -1, "peter": -1}
+        }
+    ]},
+]
